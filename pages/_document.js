675a0756e4519217596d5b9e17327fc377673b69
Heritiana <heritiana.ratsimbazafy99@gmail.com>
2026-03-24 13:19:26 +0300

    feat: add Trio favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <meta name="theme-color" content="#0b3d91" />
+      </Head>
       <body>
         <Main />
         <NextScript />
